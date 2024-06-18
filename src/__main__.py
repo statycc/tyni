0393b145db0a2c506d044f6a2470f772496564c0
Neea <neearusch@gmail.com>
2024-06-18 17:26:34 -0400

    refactoring

--- src/__main__.py
+++ src/__main__.py
@@ -7,7 +7,7 @@ from antlr4 import *
 from src import JavaLexer, JavaParser, JavaParserVisitor
 
 
-def main(input_file, out_file):
+def analyze(input_file, out_file):
     input_stream = FileStream(input_file)
     lexer = JavaLexer(input_stream)
     stream = CommonTokenStream(lexer)
@@ -16,13 +16,31 @@ def main(input_file, out_file):
         print("syntax errors")
     else:
         tree = parser.compilationUnit()
-        visitor = VisitorInterp()
-        visitor.visit(tree)
-        result = {'in': input_file, 'mtx': visitor.result}
-        save_result(out_file, result)
+        methods = MethodVisitor().visit(tree)
+        data = {'input_prog': input_file, 'result': methods.result}
+        save_result(out_file, data)
 
 
-class VisitorInterp(JavaParserVisitor):
+class ExtVisitor(JavaParserVisitor):
+    def visit(self, tree):
+        super().visit(tree)
+        return self
+
+
+class IdVisitor(ExtVisitor):
+    """Finds identifiers in a parse tree"""
+
+    def __init__(self):
+        self.vars = {}
+
+    def visitIdentifier(self, ctx: JavaParser.IdentifierContext):
+        super().visitIdentifier(ctx)
+        name = ctx.getText()
+        self.vars[name] = name
+
+
+class MethodVisitor(ExtVisitor):
+    """Visits each class method"""
 
     def __init__(self):
         self.result = {}
@@ -35,70 +53,86 @@ class VisitorInterp(JavaParserVisitor):
         ret_type = ctx.typeTypeOrVoid().getText()
         name = ctx.identifier().getText()
         body = ctx.methodBody()
-        params = IdVisitor().visit(ctx.formalParameters())
-        b_visit = RecMethodVisitor(params)
-        b_visit.visit(body)
+        b_visit = RecVisitor().visit(body)
         self.record(
             name,
             # 0: visitBlockStatement
-            # 1: method body
+            # 1: method body -- it is not necessary to keep this
             body=body.getChild(0).getChild(1).getText(),
             ret_type=ret_type,
             variables=list(b_visit.vars),
             matrix=b_visit.matrix)
 
 
-class IdVisitor(JavaParserVisitor):
-
-    def __init__(self):
-        self.vars = {}
-
-    def visit(self, tree):
-        super().visit(tree)
-        return self.vars
-
-    def visitIdentifier(self, ctx: JavaParser.IdentifierContext):
-        super().visitIdentifier(ctx)
-        name = ctx.getText()
-        self.vars[name] = name
-
-
-class RecMethodVisitor(JavaParserVisitor):
+class RecVisitor(ExtVisitor):
+    """Recursively process method body"""
 
     def __init__(self, init_vars=None):
         self.vars = init_vars or {}
         self.matrix = {}
 
-    def visitBlockStatement(
-            self, ctx: JavaParser.BlockStatementContext):
-        super().visitBlockStatement(ctx)
-        print('-' * 40)
+    @staticmethod
+    def merge(target, *args):
+        [target.update(m) for m in args]
+
+    @staticmethod
+    def occurs(exp: JavaParser.ExpressionContext):
+        return IdVisitor().visit(exp).vars
+
+    def in_vars(self):
+        return list(self.matrix.keys())
+
+    def out_vars(self):
+        return list(self.matrix.values())
 
     def visitMethodCall(self, ctx: JavaParser.MethodCallContext):
         super().visitMethodCall(ctx)
-        print('method call:', ctx.getText())
+        print('(!) method call:', ctx.getText())
 
     def visitStatement(self, ctx: JavaParser.StatementContext):
-        if ctx.IF():
-            self.__if(ctx)
-        else:
-            print(ctx.getText())
+        if ctx.blockLabel:
+            print('block:', ctx.getText())
             super().visitStatement(ctx)
+        elif ctx.IF():
+            self.__if(ctx)
+        elif ctx.WHILE():
+            self.__while(ctx)
+        elif ctx.statementExpression:
+            print('stmt exp', ctx.getText())
+            super().visitStatement(ctx)
+        elif ctx.switchExpression():
+            print('switch:', ctx.getText())
+            super().visitStatement(ctx)
+        elif ctx.identifierLabel:
+            print('id label:', ctx.getText())
+            super().visitStatement(ctx)
+        else:
+            print('other', ctx.getText())
+            super().visitStatement(ctx)
+        print('-' * 40)
+
+    def __stmt_exp(self, ctx: JavaParser.StatementContext):
+        print('stmt exp', ctx.getText())
+        super().visitStatement(ctx)
 
     def __if(self, ctx: JavaParser.StatementContext):
-        cond = ctx.getChild(1)
-        occ = self.occurs(cond)
-        true_branch = ctx.getChild(2).getText()
-        else_branch = None if ctx.getChildCount() < 5 \
-            else ctx.getChild(4).getText()
-        print("if stmt:",
-              cond.getText(), '->', list(occ),
-              '\n', true_branch, '\n', else_branch)
+        exp, tb = ctx.getChild(1), ctx.getChild(2)
+        exv, fbv = self.occurs(exp), RecVisitor()
+        tbv = RecVisitor().visit(tb)
+        if ctx.getChildCount() <= 4:
+            fb = ctx.getChild(4)
+            fbv.visit(fb)
+        self.merge(self.vars, exv, tbv.vars, fbv.vars)
 
-    def occurs(self, exp: JavaParser.ExpressionContext):
-        occ_vars = IdVisitor().visit(exp)
-        self.vars.update(occ_vars)
-        return occ_vars
+    def __while(self, ctx: JavaParser.StatementContext):
+        exp, body = ctx.getChild(1), ctx.getChild(2)
+        ex_vars = self.occurs(exp)
+        RecVisitor().visit(body)
+        self.merge(self.vars, ex_vars)
+
+    def visitExpression(self, ctx: JavaParser.ExpressionContext):
+        super().visitExpression(ctx)
+        print("exp", ctx.getText())
 
 
 def default_out(input_file: str) -> str:
@@ -117,4 +151,4 @@ def save_result(file_name: str, file_content: dict):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], default_out(sys.argv[1]))
+    analyze(sys.argv[1], default_out(sys.argv[1]))
